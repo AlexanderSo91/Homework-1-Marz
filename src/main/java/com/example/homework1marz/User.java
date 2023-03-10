451f136654371fa22fe4e7da73fb0d43cb040c65
zotin <zotinalexander@gmail.com>
2023-03-10 11:01:22 +0100

    commit

--- src/main/java/com/example/homework1marz/User.java
+++ src/main/java/com/example/homework1marz/User.java
@@ -1,21 +1,57 @@
 package com.example.homework1marz;
 
+import java.util.Objects;
+
 public class User {
 
-    private int login;
+    private String login;
+    private String email;
 
-    public User(int login, int email) {
-        this.login = login;
-        this.email = email;
+    public User() {
+        }
+
+        public User(String login, String email) {
+            if (!Objects.equals(login, email)) {
+                setLogin(login);
+                setEmail(email);
+            }
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public void setLogin(String login) {
+            if (!Objects.equals(login, this.email) && !login.isEmpty()) {
+                this.login = login;
+            } else {
+                throw new IllegalArgumentException("No correct login.");
+            }
+
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            if (email.contains("@") && email.contains(".")) {
+                this.email = email;
+            } else {
+                throw new IllegalArgumentException("No correct email.");
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return Objects.equals(login, user.login) && Objects.equals(email, user.email);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(login, email);
+        }
     }
-
-    private int email;
-
-    public int getLogin() {
-        return login;
-    }
-
-    public int getEmail() {
-        return email;
-    }
-}
